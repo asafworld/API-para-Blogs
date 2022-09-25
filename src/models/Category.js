c8d4e7cd7a95d3093f464b25515b7413b166aa67
Gabriel Asaf <gabrielasaf10@gmail.com>
2022-09-25 20:03:12 -0300

    Cria a model para categories

---
+++ src/models/Category.js
@@ -0,0 +1,24 @@
+const categorySchema = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    'Category',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        defaultValue: false,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      } 
+    },
+    {
+      timestamps: false,
+    }
+  )
+
+  return Category;
+};
+
+module.exports = categorySchema;
