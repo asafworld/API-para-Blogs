dfb1d81767e0287975c414ba6487c6c8a501d733
Gabriel Asaf <gabrielasaf10@gmail.com>
2022-09-25 13:43:10 -0300

    Sobe o middleware de verificação do token que está em desenvolvimento.

---
+++ src/middlewares/checkToken.middleware.js
@@ -0,0 +1,26 @@
+require('dotenv/config');
+const jwt = require('jsonwebtoken');
+const { User } = require('../models/index');
+
+const secret = process.env.JWT_SECRET || 'suaSenhaSecreta';
+
+const checkToken = async (req, res, next) => {
+  const { authorization } = req.headers;
+  if (authorization === null) {
+    return res.status(401).json({
+      message: 'Token not foung',
+    });
+  }
+  try {
+    const decoded = jwt.verify(authorization, secret);
+    const check = await User.findOne({ where: { email: decoded.data.email } });
+    if (!check) {
+      return res.status(401).json({ message: 'Expired or invalid token' });
+    }
+    return next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
+
+module.exports = checkToken;
